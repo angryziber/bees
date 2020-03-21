2176fa2e0f48f33b5933522a4642c6a780cf4338
Anton Keks <anton@azib.net>
2020-03-21 13:54:00 +0200

    can choose num ants

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
-	import Board from './Board.svelte'
+  import Board from './Board.svelte'
+	let numAnts = 5
 </script>
 
+<style>
+	h1 input {
+		width: 3em;
+	}
+</style>
+
 <header>
-	<h1>Ants</h1>
+  <h1>
+    Ants
+		<input type="number" bind:value={numAnts}>
+  </h1>
 </header>
+
 <main>
-	<Board size={{x: 800, y: 600}} numAnts={5}/>
+  <Board size={{x: 800, y: 600}} {numAnts}/>
 </main>
